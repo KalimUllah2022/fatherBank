0c2c09db7c598a97ec88346d832a8e0936d98818
KalimUllah2022 <villageprogrammer2022@gmail.com>
2022-02-13 14:20:19 +0600

    commit

---
+++ src/js/bank.js
@@ -0,0 +1,12 @@
+// Login Form Function
+document.getElementById('loginBtn').addEventListener('click', function() {
+    const emailField = document.getElementById('user-email');
+    const userEmail = emailField.value;
+    // console.log(userEmail);
+    const passwordField = document.getElementById('user-password');
+    const userPassword = passwordField.value;
+    // console.log(userPassword);
+    if (userEmail == 'kalim@kalim.com' && userPassword == 'secret') {
+        window.location.href = 'banking.html'
+    }
+});
